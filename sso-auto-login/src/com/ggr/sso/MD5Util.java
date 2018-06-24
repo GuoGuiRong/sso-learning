55a88835f3c3879d21d334df2640c31523cf2d81
GuoGuiRong <3095764372@qq.com>
2018-06-24 23:10:47 +0800

    编写简单的Md5工具

---
+++ sso-auto-login/src/com/ggr/sso/MD5Util.java
@@ -0,0 +1,35 @@
+package com.ggr.sso;
+
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+public class MD5Util {
+
+	public static String  toMd5(String origin) throws RuntimeException{
+		MessageDigest messageDigest = null;
+		try {
+			messageDigest = MessageDigest.getInstance("MD5");
+		} catch (NoSuchAlgorithmException e) {
+			throw new RuntimeException(e);
+		}
+		byte[] result = messageDigest.digest(origin.getBytes());
+		return toHex(result);	
+	}
+	
+	public static String toHex(byte[] result){
+		if(result==null || result.length==0){
+			return null;
+		}
+		StringBuilder stringBuilder = new StringBuilder();
+		for(int i=0;i<result.length;i++){
+			int hi = (result[i]>>4) & 0x0f;
+			int lo = result[i] & 0x0f;
+			stringBuilder.append(Character.forDigit(hi, 16)).append(Character.forDigit(lo, 16));
+		}
+		return stringBuilder.toString();
+	}
+	
+	public static void main(String[] args) {
+		System.out.println(MD5Util.toMd5("ggr"));
+	}
+}
